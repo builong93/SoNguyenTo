6aead3da5cd6d72d50642b916b81e8c72326f3e6
Bùi Duy Long <longduybui111093@gmail.com>
2024-10-03 14:35:59 +0700

    Initial commit

---
+++ primeNumbers.dart
@@ -0,0 +1,14 @@
+void main() {
+  for (int i = 2; i <= 100; i++) {
+    bool isPrime = true;
+    for (int j = 2; j <= i ~/ 2; j++) {
+      if (i % j == 0) {
+        isPrime = false;
+        break;
+      }
+    }
+    if (isPrime) {
+      print(i);
+    }
+  }
+}
